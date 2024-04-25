1dae3e21000e1d3412b3e2246acc7e8a43a493c9
Juwal86 <ju.waltregny@hotmail.com>
2024-04-25 13:14:26 +0300

    static list creation

--- app/susaf/page.tsx
+++ app/susaf/page.tsx
@@ -1,3 +1,20 @@
+import { list } from "postcss";
+import { Impact } from "../lib/definitions";
+import ImpactList from "../ui/static_list/list";
+
 export default async function Page() {
-    return <h1>Home</h1>
+    let myMap = new Map()
+    myMap.set('id', '2');
+    myMap.set('title', "Jobs created");
+    myMap.set('description', "More jobs are created");
+    myMap.set('pos_neg', 1);
+    myMap.set('dimension', "Economic")
+
+    const listOfImpacts = []
+    listOfImpacts.push(myMap)
+
+    return <div>
+        <h1>Home</h1>
+        <ImpactList impacts={listOfImpacts}/> 
+    </div>
 }
