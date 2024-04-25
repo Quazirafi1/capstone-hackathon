13a3c6c6bb3760d13bf3e0c2a8af54cacb8175b4
RubenH11 <r.c.huygens@student.vu.nl>
2024-04-25 13:19:58 +0300

    TextInput, Button and almost done with Card

---
+++ app/ui/susaf/card.tsx
@@ -0,0 +1,19 @@
+export function Card({ leading, title, children, trailing }: {
+    leading?: React.ReactNode,
+    title: string,
+    children?: React.ReactNode,
+    trailing?: React.ReactNode
+}) {
+    return (
+        <div className='flex p-1 border border-grey-500 m-1 p-5 rounded-lg shadow-md'>
+            {leading && <div className='flex w-[50px] justify-center'>{leading}</div>}
+            {leading && <div className='w-[10px]'></div>}
+            <div className='flex-grow'>
+                <div className=''>{title}</div>
+                <div>{children}</div>
+            </div>
+            {trailing && <div className='w-[10px]'></div>}
+            {trailing && <div className='flex w-[50px] justify-center'>{trailing}</div>}
+        </div>
+    );
+}
---
+++ app/ui/susaf/textinput.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+export default function TextInput({ id, label, placeholder, defaultValue, input_width_percentage }: { id: string, label?: string, placeholder: string, defaultValue?: string, input_width_percentage?: number }) {
+    input_width_percentage = input_width_percentage ?? 100;
+
+    return (
+        <div className="relative flex items-center p-4">
+            {label && (
+                <div className="flex-grow p-[9px]">{label}</div>
+            )}
+            <div className={`w-[${input_width_percentage}%]`}>
+                <input
+                    id={id}
+                    className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                    placeholder={placeholder}
+                    defaultValue={defaultValue}
+                />
+            </div>
+        </div>
+    );
+}
