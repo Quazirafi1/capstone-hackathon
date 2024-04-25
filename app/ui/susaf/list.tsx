e0fcec7b15b70c7c3eabf59bcd7eeb37ac1bd88e
Juwal86 <ju.waltregny@hotmail.com>
2024-04-25 15:04:57 +0300

    remove default of impactlist

--- app/ui/susaf/list.tsx
+++ app/ui/susaf/list.tsx
@@ -2,7 +2,7 @@ import { Impact } from '@/app/lib/definitions';
 // import React from 'react';
 
 //should get a list of type Impact
-export default function ImpactList ({impacts}:{impacts:any[]}) {
+export function ImpactList ({impacts}:{impacts:any[]}) {
    return(
     <div>{
         impacts.map((impact:any, index:number) => {
